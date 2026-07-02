7c9a5d923b944813cd6ace5bbb4765320359d02d
SumitVankar2005 <sumitvankar0039@gmail.com>
2026-07-02 22:02:34 +0530

    railway db connected

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,10 +1,18 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 
-const pool = mysql.createPool({
+const pool = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
+pool.connect((err) => {
+  if (err) {
+    console.log("DB Connection Failed:", err);
+  } else {
+    console.log("DB Connected ✅");
+  }
+});
 module.exports = pool;
