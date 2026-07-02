d5125495a50f20d2ce493dd8d3b464056ab45589
SumitVankar2005 <sumitvankar0039@gmail.com>
2026-07-02 17:14:29 +0530

    Updated Files

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "https://fin-flow-khaki.vercel.app",
+  origin: "https://fin-flow-two-nu.vercel.app",
   credentials: true
 }));
 app.use(express.json());
