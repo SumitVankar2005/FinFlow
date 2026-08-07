5d4fb5848b673e96d50ee22646975cc79af55ab7
SumitVankar2005 <sumitvankar0039@gmail.com>
2026-08-07 17:40:45 +0530

    title changed

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,22 +1,17 @@
-const mysql = require("mysql2/promise");
+const { Pool } = require("pg");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-pool.getConnection()
-  .then(conn => {
+// Test connection
+pool.connect()
+  .then(client => {
     console.log("DB Connected ✅");
-    conn.release();
+    client.release();
   })
   .catch(err => {
     console.error("DB Connection Failed:", err.message);
